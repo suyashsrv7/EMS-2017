98b0aae3fa7be635eecefa2ab08fbfe542b0a1aa
nidhi1999 <nidfhi@gmail.com>
2017-10-29 01:29:12 +0530

    provide link to crud operations of admin

---
+++ admin_dashboard.php
@@ -0,0 +1,9 @@
+<a href="event_status.php">Approve/Deny Events</a>
+<br>
+<a href="delete_organiser.php">Delete any Event Organiser</a>
+<br>
+<a href="view_registrations.php">view registrations in any event</a>
+<br>
+<a href="delete_ongoing_event.php">Delete any ongoing event</a>
+<br>
+
